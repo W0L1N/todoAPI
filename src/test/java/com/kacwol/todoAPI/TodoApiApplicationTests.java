71d48f26e5f80b1f4db1c98572a6eaeaa9178014
Kacper Wolinski <kacperwolinski404@gmail.com>
2022-04-17 19:45:02 +0200

    initial commit

---
+++ src/test/java/com/kacwol/todoAPI/TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kacwol.todoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
